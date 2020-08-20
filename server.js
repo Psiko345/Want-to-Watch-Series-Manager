21c5a9c8e2368bcf30289511e182a962576b42b7
Antonio Guedes <gguedesantonio@gmail.com>
2020-08-20 21:25:27 +1000

    Merge with suplemental file

--- server.js
+++ server.js
@@ -1,41 +1,36 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
+// Requiring necessary npm packages
 const express = require("express");
+const session = require("express-session");
+// Requiring passport as we've configured it
+const passport = require("./config/passport");
 
-// Sets up the Express App
-// =============================================================
-const app = express();
+// Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
-
-// Requiring our models for syncing
 const db = require("./models");
 
-// Sets up the Express app to handle data parsing
+// Creating express app and configuring middleware needed for authentication
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Set Handlebars.
-const exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Static directory
 app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Routes
-// =============================================================
-require("./routes/?")(app);
-require("./routes/?")(app);
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({ force: true }).then(() => {
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
-    console.log("App listening on PORT " + PORT);
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
 });
