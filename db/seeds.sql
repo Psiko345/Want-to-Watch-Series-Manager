cc30b023e42d43f7c45ecb115a38e0fec73184de
Antonio Guedes <gguedesantonio@gmail.com>
2020-08-20 19:53:27 +1000

    Sequelize added to code.

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO userInfo (seriesUUID, email)
+VALUES ("user1", "12345", "user1@test.com");
+
+INSERT INTO userInfo (username, password, email)
+VALUES ("user2", "23456", "user2@test.com");
+
+INSERT INTO userInfo (username, password, email)
+VALUES ("user3", "34567", "user3@test.com");
+
+INSERT INTO tvShows (seriesUUID, userID)
+VALUES ("tv001", 1);
+
+INSERT INTO tvShows (seriesUUID, userID)
+VALUES ("tv002", 2);
+
+INSERT INTO tvShows (seriesUUID, userID)
+VALUES ("tv003", 3);
