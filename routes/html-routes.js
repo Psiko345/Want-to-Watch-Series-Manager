1e4b4904be6399aa9a914ed729bd6835eb97411b
roukell <62136980+roukell@users.noreply.github.com>
2020-08-27 21:14:32 +1000

    update findTvShows UUID

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,6 @@
 // Requiring our custom middleware for checking if a user is logged in
+const db = require("../models");
+const tvshows = require("../models/tvshows");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
@@ -23,17 +25,27 @@ module.exports = function(app) {
   // merged all "/members" API calls
   app
     .route("/members")
-    .get((req, res) => {
+    .get(async (req, res) => {
+      const tvShows = await db.tvShows.findAll({
+        where: {
+          UserId: req.user.id
+        }
+      });
+      console.log("tvShows");
       res.render("members", {
-        user: req.user
+        user: req.user,
+        tvShows: tvShows
       });
       res.end();
     })
     .post((req, res) => {
-      console.log("got data ok");
-      console.log(req.body);
-      console.log(req.user);
-      // req.body prints imdbID which is the movie ID that we want to save in SeriesDB
+      // console.log("got data ok");
+      // console.log(req.body);
+      // console.log(req.user);
+      db.tvShows.create({
+        seriesUUID: req.body.imdbID,
+        UserId: req.user.id
+      });
       res.end();
     });
 };
